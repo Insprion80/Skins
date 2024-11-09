45baaa5d1775dd9a5f3f9bc0827ea7da2f6adcf2
Insprion80 <110155731+Insprion80@users.noreply.github.com>
2024-11-10 00:38:52 +0200

    Update installer.sh

--- xDreamy/installer.sh
+++ xDreamy/installer.sh
@@ -1,53 +1,66 @@
 #!/bin/bash
 #
 echo "------------------------------------------------------------------------"
-echo "                            Welcome To XDREAMY                          "
-echo "                         XDREAMY Skin By Inspiron                       "
-echo "                       Don't Remove this Disclaimer                     "
+echo "                            Welcome To XDREAMY                          "
+echo "                         XDREAMY Skin By Inspiron                       "
+echo "                       Don't Remove this Disclaimer                     "
 echo "------------------------------------------------------------------------"
 echo "         Experience Enigma2 skin like never before with XDREAMY         "
 echo "------------------------------------------------------------------------"
 sleep 2
-echo " removing previous version of xDreamy-FHD...      "
+echo " removing the previous version of XDREAMY Skin...      "
 sleep 2;
 if [ -d /usr/share/enigma2/xDreamy ] ; then
-opkg remove enigma2-plugin-skins-xDreamy
-rm -rf /usr/share/enigma2/xDreamy  > /dev/null 2>&1
-echo 'Package removed.'
+    opkg remove enigma2-plugin-skins-xDreamy
+    rm -rf /usr/share/enigma2/xDreamy > /dev/null 2>&1
+    echo 'Package removed.'
 else
-echo "You do not have previous version "
+    echo "You do not have the previous version "
 fi
 echo ""
+
+# Install curl if not already installed
 opkg install curl
 sleep 2
+
 #
 cd /tmp
-curl -s -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/Insprion80/Skins/main/xDreamy/xDreamy.ipk" > /tmp/xDreamy.ipk
+echo "Downloading XDREAMY skin package..."
+curl -s -k -L "https://raw.githubusercontent.com/Insprion80/Skins/main/xDreamy/xDreamy.ipk" -o /tmp/xDreamy.ipk
+if [ $? -ne 0 ]; then
+    echo "Error downloading the XDREAMY skin package."
+    exit 1
+fi
 sleep 1
-echo "installing ...."
-cd /tmp
-opkg install --force-overwrite xDreamy.ipk
-echo ""
+
+echo "Installing ...."
+opkg install --force-overwrite /tmp/xDreamy.ipk
+if [ $? -ne 0 ]; then
+    echo "Error installing the XDREAMY skin."
+    exit 1
+fi
+
 echo ""
 echo ""
 echo ""
 sleep 1
-cd
-rm -f /tmp/xDreamy.ipk
+
+# Clean up the temporary file
+if [ -f /tmp/xDreamy.ipk ]; then
+    rm -f /tmp/xDreamy.ipk
+fi
+
 echo "OK"
 #
-echo "----------------------------------------------------"
-echo "                  CONGRATULATIONS                   "
-echo "        XDREAMY Skin Installed Successfully         "
-echo "----------------------------------------------------"
-echo "   "
+echo "------------------------------------------------------------------------"
+echo "                            CONGRATULATIONS                             "
+echo "                  XDREAMY Skin Installed Successfully                   "
+echo "------------------------------------------------------------------------"
+echo "   "
 sleep 2
 echo "Please wait to restart your GUI "
-echo "   "
+echo "   "
 sleep 2
 killall -9 enigma2
-echo "   "
+echo "   "
 exit 0
-
-
-
